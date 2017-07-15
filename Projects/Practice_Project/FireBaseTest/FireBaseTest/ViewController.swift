ecfa44dfd40305cf63f43c6aa9c05e16c3d8e497
toygift <toygift@me.com>
2017-07-15 16:41:40 +0900

    Firebase Practice

---
+++ Projects/Practice_Project/FireBaseTest/FireBaseTest/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  FireBaseTest
+//
+//  Created by jaeseong on 2017. 7. 15..
+//  Copyright © 2017년 jaeseong. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class ViewController: UIViewController {
+    
+    //Firebase
+    var refer: DatabaseReference!
+    
+    //Outlet 
+    
+    @IBOutlet weak var inputName: UITextField!
+    @IBOutlet weak var inputValue: UITextField!
+    @IBOutlet weak var deleteValue: UITextField!
+    @IBOutlet weak var selectName: UITextField!
+    @IBOutlet weak var selectValue: UITextField!
+    
+    @IBAction func clickInsert(_ sender: UIButton) {
+        
+        let itemRef = refer.child(((inputName).text?.lowercased())!)
+        itemRef.setValue(inputValue.text!)
+        
+        self.inputName.text = ""
+        self.inputValue.text = ""
+        
+    }
+    
+    
+    @IBAction func clickDelete(_ sender: UIButton) {
+        let itemRef = refer.child((deleteValue.text?.lowercased())!)
+        itemRef.removeValue { (error, ref) in
+            if error != nil {
+                print("!!")
+            }
+            self.deleteValue.text = ""
+        }
+    }
+    
+    
+    @IBAction func clickSelect(_ sender: UIButton) {
+        let itemRef = refer.child((selectName.text?.lowercased())!)
+        itemRef.observeSingleEvent(of: .value, with: { (DataSnapShot) in
+            let rValue = DataSnapShot.value as? String
+        
+            self.selectValue.text = rValue
+            self.selectName.text = ""
+            //dictionary    형태로 가지고 와서
+
+        })
+        
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Firebase
+        refer = Database.database().reference()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
